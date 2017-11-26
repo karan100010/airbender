2383dad75b20394acf00109c8e09067dd9178573
Arjun Venkatraman <arjun@mojolab.org>
2017-11-26 17:20:14 +0530

    Added graph axes; Line not showing

--- web/js/graph.js
+++ web/js/graph.js
@@ -46,7 +46,52 @@ function getSiPm10(pm10){
 		
 
 
-
+//Functions to calculate aqi
+		
+		
+function getLatestAvgs(id,channeldata){
+		channeldata=channeldata.slice(-10)
+		sumsipm25=0.0
+		sumsipm10=0.0
+		sumaqi=0.0
+		sumpm10=0
+		sumpm25=0
+		for (row in channeldata){
+			
+			pm25=parseFloat(channeldata[row].pm25)
+			pm10=parseFloat(channeldata[row].pm10)
+			sumpm10=sumpm10+pm10
+			sumpm25=sumpm25+pm25
+			sipm25=getSiPm25(pm25)
+			sipm10=getSiPm10(pm10)
+			sumsipm10=sumsipm10+sipm10
+			sumsipm25=sumsipm25+sipm25
+			if(sipm10>sipm25){
+				aqi=sipm10;
+			}
+			else{
+				aqi=sipm25;
+			}
+			//console.log("PM10:"+pm10+" SIPM10:"+sipm10+" PM25: "+pm25+" SIPM25:"+sipm25+" AQI: "+aqi)
+			sumaqi=sumaqi+aqi
+			ts=channeldata[row].created_at
+		}
+		ts=channeldata[row].created_at
+		
+		avgsipm25=sumsipm25/10
+		avgsipm10=sumsipm10/10
+		avgaqi=sumaqi/10
+		avgpm10=sumpm10/10
+		avgpm25=sumpm25/10
+		response={}
+		response['avgaqi']=Math.round(avgaqi)
+		response['avgsipm25']=Math.round(avgsipm25)
+		response['avgsipm10']=Math.round(avgsipm10)
+		response['avgpm10']=Math.round(avgpm10)
+		response['avgpm25']=Math.round(avgpm25)
+		response['ts']={"date":ts.split("T")[0],"time":ts.split("T")[1].split("Z")[0]}
+		return response
+}
 
 
 function displayGraph(div,data=null){
@@ -80,8 +125,8 @@ function displayGraph(div,data=null){
 	//console.log(d3.time.hour.floor(new Date()))
 	//console.log(d3.time.hour.ceil(new Date()))
 		
-	var x = d3.scaleTime().range([0, pm10.width-35]);  
-	var y = d3.scaleLinear().range([gheight, 0]);
+	var x = d3.scaleTime().range([0, pm10.width-40]);  
+	var y = d3.scaleLinear().range([gheight-25, 0]);
 
 
 	height=gheight-25
@@ -90,29 +135,41 @@ function displayGraph(div,data=null){
 		d.created_at = parseTime(d.created_at);
 		d.pm10 = +d.pm10;
 		d.pm25 = +d.pm25;
-		d.aqi = +d.aqi;
 		d.sipm10=Math.round(getSiPm10(d.pm10));
-		d.simp25=Math.round(getSiPm25(d.pm25));
+		d.sipm25=Math.round(getSiPm25(d.pm25));
 		if(d.sipm10>d.sipm25){
 			d.aqi=d.sipm10;
 		}
 		else{
 			d.aqi=d.sipm25;
 		}
+		d.aqi = +d.aqi;
 		
     });
-    
+    console.log(data)
     x.domain(d3.extent(data, function(d) { return d.created_at; }));
     y.domain([0, d3.max(data, function(d) { return d.aqi; })]);
-
+	// Define the line
+	var pm10line = d3.line()    
+		.x(function(d) { return x(d.created_at); })
+		.y(function(d) { return y(d.pm10); });
+	
+	svgpm10=d3.select(div).select("#svgpm10").append("g")
+	data.forEach(function(d){
+		//console.log(d)
+		//console.log(x(d.created_at)+","+y(d.pm10));
+		svgpm10		
+		.append("path")
+		.attr("class","line")
+		.attr("d",pm10line(d));
+	});
 	
 	
-	
-	
+		
 	d3.select(div).select("#svgpm10")			
 		.append("g")
 		.attr("class", "axis")
-		.attr("transform", "translate(20," + height + ")")
+		.attr("transform", "translate(30," + height + ")")
 		.call(d3.axisBottom(x)
               .tickFormat(d3.timeFormat("%H:%M")))
       .selectAll("text")	
@@ -120,11 +177,24 @@ function displayGraph(div,data=null){
         .attr("dx", "-.08em")
         .attr("dy", ".075em")
         .attr("transform", "rotate(-35)");
+    
+    
+    d3.select(div).select("#svgpm10")			
+		.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(30,0)")
+		.call(d3.axisLeft(y)
+				.ticks(4))
+	  .selectAll("text")	
+        .style("text-anchor", "end")
+        .attr("dx", "-.08em")
+        .attr("dy", ".075em")
+        .attr("transform", "rotate(-45)");
 
 	d3.select(div).select("#svgpm25")			
 		.append("g")
 		.attr("class", "axis")
-		.attr("transform", "translate(20," + height + ")")
+		.attr("transform", "translate(30," + height + ")")
 		.call(d3.axisBottom(x)
               .tickFormat(d3.timeFormat("%H:%M")))
       .selectAll("text")	
@@ -133,10 +203,22 @@ function displayGraph(div,data=null){
         .attr("dy", ".075em")
         .attr("transform", "rotate(-35)");
 
+	 d3.select(div).select("#svgpm25")			
+		.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(30,0)")
+		.call(d3.axisLeft(y)
+				.ticks(4))
+	  .selectAll("text")	
+        .style("text-anchor", "end")
+        .attr("dx", "-.08em")
+        .attr("dy", ".075em")
+        .attr("transform", "rotate(-45)");
+		
 	d3.select(div).select("#svgaqi")			
 		.append("g")
 		.attr("class", "axis")
-		.attr("transform", "translate(20," + height + ")")
+		.attr("transform", "translate(30," + height + ")")
 		.call(d3.axisBottom(x)
               .tickFormat(d3.timeFormat("%H:%M")))
       .selectAll("text")	
@@ -145,6 +227,16 @@ function displayGraph(div,data=null){
         .attr("dy", ".075em")
         .attr("transform", "rotate(-35)");
         
-    
+    d3.select(div).select("#svgaqi")			
+		.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(30,0)")
+		.call(d3.axisLeft(y)
+				.ticks(4))
+	  .selectAll("text")	
+        .style("text-anchor", "end")
+        .attr("dx", "-.08em")
+        .attr("dy", ".075em")
+        .attr("transform", "rotate(-45)");
 
 }
