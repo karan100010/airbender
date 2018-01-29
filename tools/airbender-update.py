726ef1556bf5658b331b800048223368b5c46c56
Arjun Venkatraman <arjun@mojolab.org>
2018-01-29 14:41:40 +0530

    Updated

--- tools/airbender-update.py
+++ tools/airbender-update.py
@@ -2,72 +2,27 @@ import os,sys
 
 sys.path.append("/opt/livingdata/lib")
 sys.path.append("/opt/SoMA/python/lib")
-from livdattable import *
+from livdatbutler import *
 from libsoma import *
-import pygsheets
+
 from bs4 import BeautifulSoup
 
-class AirBender(SoMACyborg):
-	def __init__(self,configfile,headless=False):
-		config=ConfigParser.ConfigParser()
-		config.read(configfile)
-		self.config=config
+# Extend init with *args and **kwargs
+
+class AirBender(DataButler):
+	def __init__(self,*args, **kwargs):
+		super(AirBender,self).__init__(*args, **kwargs)
+		self.airvedausername = self.config.get("Airveda","username")
+		self.airvedapassword = self.config.get("Airveda","password")
+		self.airvedadevicelist = self.config.get("Airveda","devicelist")
+		counter=0
 		try:
-			self.fbusr = config.get("User","fbusername")
-			self.fbpwd = config.get("User","fbpassword")
+			print "Trying to get airvedadevsheet"
+			self.airvedadevsheet=self.gc.open_by_key(self.airvedadevicelist)
 		except:
-			print "Cyborg did not have an FB id configured"
-		try:
-			self.googleusr = config.get("User","googleusername")
-			self.googlepwd = config.get("User","googlepassword")
-		except:
-			print "Cyborg did not have an Google id configured"
-		try:
-			self.outhstore = config.get("Google","outhstore")
-			self.outhfile = config.get("Google","outhfile")
-		except:
-			print "Cyborg did not get a google client secret"
+			counter +=1
 			
 		
-		self.airvedausername = config.get("Airveda","username")
-		self.airvedapassword = config.get("Airveda","password")
-		self.airvedadevicelist = config.get("Airveda","devicelist")
-		self.gc=pygsheets.authorize(outh_file=self.outhfile,outh_nonlocal=True,outh_creds_store=self.outhstore)
-		self.airvedadevsheet=self.gc.open_by_key(self.airvedadevicelist)
-		
-		
-		self.datapath=config.get("System","datapath")
-		sessionpathprefix=config.get("System","sessionpathprefix")
-		ts=datetime.now()
-		sessiondir=sessionpathprefix+"-"+ts.strftime("%Y%b%d-%H%M%S")
-		self.sessionpath=os.path.join(self.datapath,sessiondir)
-		self.sessiondownloaddir=os.path.join(self.sessionpath,"downloads")
-		self.sessionjsonpath=os.path.join(self.sessionpath,"json")
-		if not os.path.exists(self.datapath):
-			os.mkdir(self.datapath)
-		if not os.path.exists(self.sessionpath):
-			os.mkdir(self.sessionpath)
-		if not os.path.exists(self.sessiondownloaddir):
-			os.mkdir(self.sessiondownloaddir)
-		if not os.path.exists(self.sessionjsonpath):
-			os.mkdir(self.sessionjsonpath)
-		
-		if headless==True:
-			os.environ['MOZ_HEADLESS'] = '1'
-		
-		firefox_profile = webdriver.FirefoxProfile()
-		firefox_profile.set_preference("browser.privatebrowsing.autostart", True)
-		firefox_profile.set_preference("browser.download.dir", self.sessiondownloaddir);
-
-		firefox_profile.set_preference("browser.download.folderList", 2);
-
-
-		firefox_profile.set_preference("browser.download.manager.showWhenStarting", False);
-
-		firefox_profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/plain, text/csv",)
-		
-		driver = webdriver.Firefox(firefox_profile=firefox_profile)
-		self.driver=driver
 	def airveda_get_dev_data(self,dev):
 		if os.path.exists(os.path.join(self.sessiondownloaddir,str(dev)+".csv")):
 			os.rename(os.path.join(self.sessiondownloaddir,str(dev)+".csv"),os.path.join(self.sessiondownloaddir,str(dev)+"-prev.csv"))
