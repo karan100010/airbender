2a71f8bab23d7bce8deaad70bdebe647ee9ec011
Arjun Venkatraman <arjun@mojolab.org>
2018-02-19 16:39:34 +0530

    Updated

--- tools/airbender-update.py
+++ tools/airbender-update.py
@@ -3,14 +3,14 @@ sys.path.append("/opt/airbender/lib")
 from airbender import *
 from livdatdatastream import *
 from ftplib import FTP, error_perm
-def placeFiles(ftp, path):
+def placeFiles(ftp, path,logger):
     for name in os.listdir(path):
         localpath = os.path.join(path, name)
         if os.path.isfile(localpath):
-            logging.info("STOR", name, localpath)
+            logger.info("STOR " + name +" " + localpath)
             ftp.storbinary('STOR ' + name, open(localpath,'rb'))
         elif os.path.isdir(localpath):
-            print("MKD", name)
+            logger.info("MKD " + name)
 
             try:
                 ftp.mkd(name)
@@ -20,29 +20,29 @@ def placeFiles(ftp, path):
                 if not e.args[0].startswith('550'): 
                     raise
 
-            print("CWD", name)
+            logger.info("CWD " + name)
             ftp.cwd(name)
             placeFiles(ftp, localpath)           
-            print("CWD", "..")
+            logger.info("CWD " + "..")
             ftp.cwd("..")
 
 host="103.21.58.231"
 port=21
 
 
-a=AirBender(sys.argv[1],headless=True)
+a=AirBender(sys.argv[1])
 a.airveda_login()
 time.sleep(10)
 
 while True:
-	logging.info("Starting update....")
+	a.logger.info("Starting update....")
 	a.reload_devdf()
 	for dev in a.devdf.devname:
 		a.update_device(dev)
 	ftp=FTP()
 	ftp.connect(host,port)
 	ftp.login("airbender@environicsindia.in","A1rbender!")
-	placeFiles(ftp,"/media/usb/airbenderdata/streamdata/")
+	placeFiles(ftp,"/media/usb/airbenderdata/streamdata/",a.logger)
 	time.sleep(1200)
 
 
