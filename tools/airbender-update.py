4cf70031ca676ae3e3ed4b97248ecea2e493bf80
Arjun Venkatraman <arjun@mojolab.org>
2018-02-19 09:53:12 +0530

    Added some functionality to test

--- tools/airbender-update.py
+++ tools/airbender-update.py
@@ -7,7 +7,7 @@ def placeFiles(ftp, path):
     for name in os.listdir(path):
         localpath = os.path.join(path, name)
         if os.path.isfile(localpath):
-            print("STOR", name, localpath)
+            logging.info("STOR", name, localpath)
             ftp.storbinary('STOR ' + name, open(localpath,'rb'))
         elif os.path.isdir(localpath):
             print("MKD", name)
@@ -26,24 +26,24 @@ def placeFiles(ftp, path):
             print("CWD", "..")
             ftp.cwd("..")
 
+host="103.21.58.231"
+port=21
 
 
 a=AirBender(sys.argv[1],headless=True)
 a.airveda_login()
 time.sleep(10)
 
-
-for dev in a.devdf.devname:
-    a.update_device(dev)
-
-
-host="103.21.58.231"
-port=21
-ftp=FTP()
-ftp.connect(host,port)
-ftp.login("airbender@environicsindia.in","A1rbender!")
-placeFiles(ftp,"/media/usb/airbenderdata/streamdata/")
-
+while True:
+	logging.info("Starting update....")
+	a.reload_devdf()
+	for dev in a.devdf.devname:
+		a.update_device(dev)
+	ftp=FTP()
+	ftp.connect(host,port)
+	ftp.login("airbender@environicsindia.in","A1rbender!")
+	placeFiles(ftp,"/media/usb/airbenderdata/streamdata/")
+	time.sleep(1200)
 
 
 #airvedafiles=a.airveda_update()
